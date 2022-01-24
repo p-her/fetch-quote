26dd6b09211279ea01fbd73a9299863027e091cf
Pao Her <p.her007@hotmail.com>
2022-01-23 20:51:00 -0600

    add comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,19 +5,6 @@ var wordDefinitionEl = document.querySelector("#definition");
 var wordSynonymEl = document.querySelector("#synonyms");
 var wordPronunciationEl = document.querySelector("#pronunciation");
 
-var storageArr = [];
-
-var locationStorage = function(word){
-    storageArr = JSON.parse(window.localStorage.getItem("word")) || [];
-    var value = word;
-    if(storageArr.indexOf(value)== -1){
-        storageArr.push(value);
-    }
-    
-    localStorage.setItem("word", JSON.stringify(storageArr));
-};
-
-
 //function to handle button click
 
 var formSubmitHandler = function(event){
@@ -25,82 +12,65 @@ var formSubmitHandler = function(event){
     var word = wordInputEl.value.trim();
     fetchAPI(word)
     
-    locationStorage(word);
     wordInputEl.value="";
-   
-
-
-    console.log(word)
-    getWordFacts(word);
 
 }
-
-
-
 
 userFormEl.addEventListener("submit", formSubmitHandler);
 
 
-
-
+// make a fetch api call to get a quote
 function fetchAPI(word){
 
+    // fetch url
     fetch("https://type.fit/api/quotes")
     .then(response => response.json())
     .then(function(data){
+        // empty array
         var arr = [];
 
+            // loop through the data
             for(var count = 0; count < data.length; count++){
 
+                // set flag to false
                 var flag = false;
 
+                // get the quote from the api and assign to str variable
                 var str = data[count].text;
-    
+
+                // split the words into an array and assign to arr
                     arr = str.split(" ");
+
+                // loop thrugh the array 
                 for(var i = 0; i < arr.length; i++){
+
+                    // check if the word the user enters match with any words of the array
+                    // if it's match, then display the quote to the user
                     if(arr[i] === word){
                         
+                        // get the quote
                        $(".quotes").text(str)
+                       // set flag to true
                         flag = true;
                         
                     }
                     
                 }
 
+                // if flag is true, then break the loop
                 if(flag){
                     break;
                 }
 
+
+                // check if no word match with the user input
+                // then display no quote to the user
                 if(count == (data.length -1)){
 
                     $(".quotes").text('No Quote')
                 }
 
-             
     
             }
-
-
     })
 }
-
-var storageArr = [];
-
-var locationStorage = function(word){
-    storageArr = JSON.parse(window.localStorage.getItem("word")) || [];
-    var value = word;
-    if(storageArr.indexOf(value)== -1){
-        storageArr.push(value);
-    }
-    
-    localStorage.setItem("word", JSON.stringify(storageArr));
-};
-
-var formSubmitHandler = function(event){
-    event.preventDefault();
-    var word = wordInputEl.value.trim();
-    locationStorage(word);
-    wordInputEl.value="";
-    console.log(word)
-}
-userFormEl.addEventListener("submit", formSubmitHandler);
